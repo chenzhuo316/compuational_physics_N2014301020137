f49f39a9839009f81ff508d0d096909dd70cea7c
李湘黔 <lxq316@outlook.com>
2016-09-26 01:07:51 +0800

    Update Homework_03.py

--- File_1/Homework_03.py
+++ File_1/Homework_03.py
@@ -1,1 +1,18 @@
-
+a="#    ###  #   # ###   #  #   # ###    ##   ###   #  #   #"
+b="#     #    # #   #   # # ##  ##      #  #   #   # # ##  #"
+c="#     #     #    #  #   ## # ## ###  ####   #  #   ## # #"
+d="#     #    # #   #  ######  ###  ##  #  ##  #  ######  ##"
+e="#### ###  #   # ### #   ##   # ## #   ##   ### #   ##   #"
+import time 
+import os 
+op=0 
+while op<120: 
+    time.sleep(0.02) 
+    i = os.system('cls') 
+    a, b,c,d,e=" "+a, " "+b, " "+c," "+d," "+e 
+    print (a) 
+    print (b) 
+    print (c) 
+    print (d) 
+    print (e) 
+    op=op+1 
